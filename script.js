68a66e62e1d99001aa9d5468251461072def53fc
Jonatan Ortiz <jonataneduard@gmail.com>
2019-12-29 16:58:04 -0300

    Resolvido zeros e pontos seguidos e erro NaN

--- script.js
+++ script.js
@@ -7,13 +7,7 @@ function printHistory(num) {
 }
 
 function getOutput() {
-	n = document.getElementById("hidden-output-value").innerText;
-	if (Number(n) >= 1 && n.substr(0, 1) == "0") {
-		return n.substr(1, 30);
-	}
-	else {
-		return n;
-	}
+		return document.getElementById("output-value").innerText;
 }
 
 function printOutput(num) {
@@ -23,19 +17,9 @@ function printOutput(num) {
 	else if (num == ".") {
 		document.getElementById("output-value").innerText = ".";
 	}
-	else if (num.endsWith(".")) {
-		document.getElementById("output-value").innerText = getFormattedNumber(num) + ".";
-	}
-	else {
-		document.getElementById("output-value").innerText = getFormattedNumber(num);
-	}
-	document.getElementById("hidden-output-value").innerText = num;
-}
-
-function getFormattedNumber(num) {
-	var n = Number(num);
-	var value = n.toLocaleString("en");
-	return value;
+	else{
+		document.getElementById("output-value").innerText = num;
+	}	
 }
 
 var clicked_number = false;
@@ -44,47 +28,45 @@ var operator = document.getElementsByClassName("operator");
 for (var i = 0; i < operator.length; i++) {
 	operator[i].addEventListener('click', function () {
 		if (this.id == "clear") {
-			clicked_number = false;
 			printHistory("");
-			printOutput("");
+			printOutput("0");
 		}
 		else if (this.id == "backspace") {
 			var output = getOutput().toString();
-			if (output) {
+			if (output!="0") {
 				output = output.substr(0, output.length - 1);
-				if (output != "") {
-					printOutput(output);
+				printOutput(output);
+				clicked_number = true;
+				if (output == "") {
+					printOutput("0");
 					clicked_number = true;
-				}
-				else {
-					clicked_number = false;
-					printOutput("")
 				}
 			}
 		}
 		else {
 			var output = getOutput();
 			var history = getHistory();
-			if (clicked_number==false) {
+			if (clicked_number==false&&this.id!= "=") {
 				history = history.substr(0, history.length - 1);
+				printHistory(getOutput()+history+this.id);
+				printOutput("");
 			}
-			if (output != "" || history != "") {
+			else if (this.id == "=") {
 				history = history + output;
-				if (this.id == "=") {
-					var result = eval(history).toString();
-					printOutput(result);
-					printHistory("");
-					clicked_number = false;
+				var result = eval(history).toString();
+				if(isNaN(result)){
+					printOutput("Indefinido");
 				}
-				else {
-					history = history + this.id;
-					printHistory(history);
-					printOutput("");
-					clicked_number = false;
+				else{
+				printOutput(result);
+				printHistory("");
 				}
 			}
-			else if (output == "" && history == "" && this.id == "-") {
-				printHistory(this.id);
+			else {
+				history = history + output + this.id;
+				printHistory(history);
+				printOutput("");
+				clicked_number=false;
 			}
 		}
 	});
@@ -93,15 +75,30 @@ for (var i = 0; i < operator.length; i++) {
 var number = document.getElementsByClassName("number");
 for (var i = 0; i < number.length; i++) {
 	number[i].addEventListener('click', function () {
-			clicked_number = true;
-			var output = getOutput();
-			output = output + this.id;
+		if (getOutput()=="0"){
+			printOutput("")
+		}
+		clicked_number = true;
+		var output = getOutput();
+		output = Number(output + this.id);
+		if(output==""){
+			printOutput("0")
+		}
+		else{
 			printOutput(output);
+		}
 	});
 }
 
 document.getElementById(".").addEventListener('click', function () {
-	var output = getOutput();
-	output = output + this.id;
-	printOutput(output);
+	if(getOutput()==""){
+		printOutput("0.");
+		clicked_number = true;
+	}
+	else if(getOutput().includes(".")){}
+	else {
+	 var output = getOutput();
+	 output = output + this.id;
+	 printOutput(output);
+	}
 });
